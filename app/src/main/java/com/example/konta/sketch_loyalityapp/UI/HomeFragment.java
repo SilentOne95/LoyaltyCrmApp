64a79ad4ff3da03f73a90a4e472e5f33b33e61f4
SilentOne95 <silentone@onet.pl>
2018-11-07 12:48:20 +0100

    Adding layout's content using local JSON files

--- app/src/main/java/com/example/konta/sketch_loyalityapp/UI/HomeFragment.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/UI/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.konta.sketch_loyalityapp.UI;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,16 +9,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import com.example.konta.sketch_loyalityapp.Utils.GridItemAdapter;
 import com.example.konta.sketch_loyalityapp.R;
 import com.example.konta.sketch_loyalityapp.Utils.Item;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     static final ArrayList<Item> itemList = new ArrayList<>();
+    private String json;
+    private String layoutTitle;
+    private int resourceSpecialOffer;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -25,11 +36,54 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        for (int i = 0; i < 10; i++) {
-            itemList.add(new Item("Product Name", R.drawable.product_image));
+        // Reading JSON file from assets
+        try {
+            InputStream inputStream = getActivity().getAssets().open("home.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        // Extracting objects that has been built up from parsing the given JSON file
+        int columns = 0;
+        try {
+            JSONObject object = new JSONObject(json);
+            JSONArray array = object.getJSONArray("offers");
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject insideObj = array.getJSONObject(i);
+                String title = insideObj.getString("offerTitle");
+                String image = insideObj.getString("offerImage");
+
+                Resources resources = this.getResources();
+                final int resourceId = resources
+                        .getIdentifier(image, "drawable", getActivity().getPackageName());
+                itemList.add(new Item(title, resourceId));
+            }
+
+            layoutTitle = object.getString("layoutTitle");
+
+            Resources resources = this.getResources();
+            String specialOfferImage = object.getString("specialImage");
+            resourceSpecialOffer = resources
+                    .getIdentifier(specialOfferImage, "drawable", getActivity().getPackageName());
+            columns = object.getInt("numColumns");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ImageView specialOfferImage = rootView.findViewById(R.id.special_offer_image);
+        specialOfferImage.setImageResource(resourceSpecialOffer);
+
         GridItemAdapter adapter = new GridItemAdapter(getActivity(), itemList);
         final GridView gridView = rootView.findViewById(R.id.grid_view);
+        gridView.setNumColumns(columns);
         gridView.setAdapter(adapter);
 
         return rootView;
@@ -39,6 +93,6 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        getActivity().setTitle("Home");
+        getActivity().setTitle(layoutTitle);
     }
 }
--- app/src/main/java/com/example/konta/sketch_loyalityapp/UI/ProductsFragment.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/UI/ProductsFragment.java
@@ -1,5 +1,6 @@
 package com.example.konta.sketch_loyalityapp.UI;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,11 +14,19 @@ import com.example.konta.sketch_loyalityapp.Utils.GridItemAdapter;
 import com.example.konta.sketch_loyalityapp.R;
 import com.example.konta.sketch_loyalityapp.Utils.Item;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class ProductsFragment extends Fragment {
 
     static final ArrayList<Item> itemList = new ArrayList<>();
+    private String json;
+    private String layoutTitle;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -25,19 +34,55 @@ public class ProductsFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_products, container, false);
 
-        for (int i = 0; i < 10; i++) {
-            itemList.add(new Item("Product Name", R.drawable.product_image));
+        // Reading JSON file from assets
+        try {
+            InputStream inputStream = getActivity().getAssets().open("products.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        // Extracting objects that has been built up from parsing the given JSON file
+        int columns = 0;
+        try {
+            JSONObject object = new JSONObject(json);
+            JSONArray array = object.getJSONArray("products");
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject insideObj = array.getJSONObject(i);
+                String title = insideObj.getString("productTitle");
+                String image = insideObj.getString("productImage");
+
+                Resources resources = this.getResources();
+                final int resourceId = resources
+                        .getIdentifier(image, "drawable", getActivity().getPackageName());
+                itemList.add(new Item(title, resourceId));
+            }
+
+            layoutTitle = object.getString("layoutTitle");
+            columns = object.getInt("numColumns");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         GridItemAdapter adapter = new GridItemAdapter(getActivity(), itemList);
         final GridView gridView = rootView.findViewById(R.id.grid_view);
+        gridView.setNumColumns(columns);
         gridView.setAdapter(adapter);
 
         return rootView;
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        getActivity().setTitle("Products");
+        getActivity().setTitle(layoutTitle);
     }
 }
