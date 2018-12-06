2b15d16739a8efee3df828e67de9ad4f7d0d310e
SilentOne95 <silentone@onet.pl>
2018-12-06 16:57:12 +0100

    Adding new layout - details view

---
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/AdditionalUI/CouponDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.konta.sketch_loyalityapp.AdditionalUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.konta.sketch_loyalityapp.R;
+
+public class CouponDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon_details);
+    }
+}
