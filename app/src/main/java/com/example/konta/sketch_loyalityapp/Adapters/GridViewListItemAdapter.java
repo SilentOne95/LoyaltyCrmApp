d4535e90db65088a2958c0944d22e1fc0aba7476
SilentOne95 <silentone@onet.pl>
2018-12-07 13:58:09 +0100

    Testing replacing text in button with listener

--- app/src/main/java/com/example/konta/sketch_loyalityapp/Adapters/GridViewListItemAdapter.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/Adapters/GridViewListItemAdapter.java
@@ -1,6 +1,7 @@
 package com.example.konta.sketch_loyalityapp.Adapters;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,12 +12,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.konta.sketch_loyalityapp.AdditionalUI.CouponDetailsActivity;
 import com.example.konta.sketch_loyalityapp.R;
 import com.example.konta.sketch_loyalityapp.ModelClasses.Item;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class GridViewListItemAdapter extends ArrayAdapter<Item> {
 
@@ -86,7 +87,8 @@ public class GridViewListItemAdapter extends ArrayAdapter<Item> {
         holder.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getContext(), CouponDetailsActivity.class);
+                getContext().startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/konta/sketch_loyalityapp/AdditionalUI/CouponDetailsActivity.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/AdditionalUI/CouponDetailsActivity.java
@@ -1,15 +1,52 @@
 package com.example.konta.sketch_loyalityapp.AdditionalUI;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.konta.sketch_loyalityapp.R;
 
-public class CouponDetailsActivity extends AppCompatActivity {
+public class CouponDetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button showCouponCodeButton;
+    GradientDrawable backgroundButton;
+    Spannable staticCodeText, promoCodeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupon_details);
+        setTitle("Details");
+
+        showCouponCodeButton = findViewById(R.id.show_coupon_button);
+        showCouponCodeButton.setOnClickListener(this);
+
+        backgroundButton = (GradientDrawable) showCouponCodeButton.getBackground();
+
+        // Setting up text which will be displayed on button when it's clicked
+        staticCodeText = new SpannableString(getResources().getString(R.string.coupon_details_code_text));
+        staticCodeText.setSpan(new ForegroundColorSpan(Color.BLACK), 0, staticCodeText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        promoCodeText = new SpannableString("CWVZ85F");
+        promoCodeText.setSpan(new ForegroundColorSpan(Color.BLUE), 0, promoCodeText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        promoCodeText.setSpan(new RelativeSizeSpan(1.2f), 0, promoCodeText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        promoCodeText.setSpan(new StyleSpan(Typeface.BOLD), 0, promoCodeText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        showCouponCodeButton.setText(TextUtils.concat(staticCodeText, "  ", promoCodeText));
+        backgroundButton.setColor(Color.parseColor("#e6e6e6"));
     }
 }
