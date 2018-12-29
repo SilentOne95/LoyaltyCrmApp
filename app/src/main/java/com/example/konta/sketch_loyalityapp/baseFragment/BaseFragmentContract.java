0feb83836041834b1db246fb0f025fc7a8e8610a
SilentOne <SilentOne@onet.pl>
2018-12-29 23:14:27 +0100

    Creating BaseFragmentContract interface

---
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/baseFragment/BaseFragmentContract.java
@@ -0,0 +1,13 @@
+package com.example.konta.sketch_loyalityapp.baseFragment;
+
+public interface BaseFragmentContract {
+
+    interface View {
+
+        void attachPresenter(Presenter presenter);
+    }
+
+    interface Presenter {
+
+    }
+}
