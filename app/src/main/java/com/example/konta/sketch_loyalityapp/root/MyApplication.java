490772e7db965a44a0ee9031de5467f162ddb955
SilentOne95 <silentone@onet.pl>
2019-01-07 17:34:27 +0100

    Testing retrofit

--- app/src/main/java/com/example/konta/sketch_loyalityapp/root/MyApplication.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/root/MyApplication.java
@@ -1,9 +1,19 @@
 package com.example.konta.sketch_loyalityapp.root;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
 
 public class MyApplication extends Application {
 
@@ -15,10 +25,33 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+        api.getTest().enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                try {
+                    Log.d("Test test ", response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         mApplicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
                 .build();
-    }
+        }
 
     public ApplicationComponent getApplicationComponent() { return mApplicationComponent; }
 
@@ -36,5 +69,10 @@ public class MyApplication extends Application {
         }
 
         return mJson;
+    }
+
+    interface Api {
+        @GET("/posts")
+        Call<ResponseBody> getTest();
     }
 }
