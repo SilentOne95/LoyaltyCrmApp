ba43e94eeef8fa2bb83b5e49fccc8ff3dfe9c381
SilentOne95 <silentone@onet.pl>
2019-01-11 17:53:29 +0100

    Refactoring Home adapter logic

--- app/src/main/java/com/example/konta/sketch_loyalityapp/ui/home/HomePresenter.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/ui/home/HomePresenter.java
@@ -1,6 +1,8 @@
 package com.example.konta.sketch_loyalityapp.ui.home;
 
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.util.SparseArray;
 
 import com.example.konta.sketch_loyalityapp.data.menu.MenuComponent;
 import com.example.konta.sketch_loyalityapp.ui.main.MainActivityContract;
@@ -20,6 +22,29 @@ public class HomePresenter implements HomeContract.Presenter, MainActivityContra
 
     @Override
     public void onFinished(List<MenuComponent> listMenuComponent) {
+
+        // TODO:
+        SparseArray<MenuComponent> list = new SparseArray<>();
+        int i = 0;
+
+        if (listMenuComponent != null) {
+            for (MenuComponent component : listMenuComponent) {
+                if (component.getList().equals("menu")) {
+                    list.append(i, component);
+                    i++;
+                    Log.d("test 1 ", component.getComponentTitle());
+                }
+            }
+
+            for (MenuComponent component : listMenuComponent) {
+                if (component.getList().equals("submenu")) {
+                    list.append(i, component);
+                    i++;
+                    Log.d("test 2 ", component.getComponentTitle());
+                }
+            }
+        }
+
         if (view != null) {
             view.setUpAdapter(listMenuComponent);
         }
