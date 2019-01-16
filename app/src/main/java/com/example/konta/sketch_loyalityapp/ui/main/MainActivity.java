de27c11e28ba03e94a71364380ca86695b61ccfc
SilentOne95 <silentone@onet.pl>
2019-01-16 13:32:28 +0100

    Refactoring method's logic that receive menu json string

--- app/src/main/java/com/example/konta/sketch_loyalityapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/ui/main/MainActivity.java
@@ -124,11 +124,11 @@ public class MainActivity extends BaseActivity implements DrawerLayout.DrawerLis
         Menu menu = mNavigationView.getMenu(), submenu = mNavigationView.getMenu();
 
         for (int i = 0; i < menuSectionArray.size(); i++) {
-            menu.add(NAV_VIEW_FIRST_GROUP_ID, i, NAV_VIEW_ORDER, menuSectionArray.get(i).getTitle());
+                menu.add(NAV_VIEW_FIRST_GROUP_ID, i, NAV_VIEW_ORDER, menuSectionArray.get(i).getTitle());
         }
 
         for (int i = 0; i < submenuSectionArray.size(); i++) {
-            submenu.add(NAV_VIEW_SECOND_GROUP_ID, i, NAV_VIEW_ORDER, submenuSectionArray.get(i).getTitle());
+                submenu.add(NAV_VIEW_SECOND_GROUP_ID, i, NAV_VIEW_ORDER, submenuSectionArray.get(i).getTitle());
         }
 
         // Set checked home screen in Navigation Drawer
--- app/src/main/java/com/example/konta/sketch_loyalityapp/ui/main/MainActivityPresenter.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/ui/main/MainActivityPresenter.java
@@ -48,35 +48,76 @@ public class MainActivityPresenter implements MainActivityContract.Presenter,
 
     @Override
     public void onFinished(List<MenuComponent> listOfItems) {
-        int menuId = 0, submenuId = 0, homeScreenId = 0;
+        int homeScreenId = 0;
         String menuType, type, title;
+        SparseArray<MenuComponent> menuTemporary = new SparseArray<>();
+        SparseArray<MenuComponent> submenuTemporary = new SparseArray<>();
 
         for (int i = 0; i < listOfItems.size(); i++) {
             menuType = listOfItems.get(i).getList();
 
-            if (menuType.equals("menu")) {
-                type = listOfItems.get(i).getType();
-                title = listOfItems.get(i).getComponentTitle();
+            switch (menuType) {
+                case "menu":
+                    menuTemporary.append(i, listOfItems.get(i));
 
-                menuArray.append(menuId, new HelperComponent(type, title));
-                menuId++;
+                    if (listOfItems.get(i).getIsHomePage().equals(1)) {
+                        homeScreenId = listOfItems.get(i).getId();
+                    }
+                    break;
+                case "submenu":
+                    submenuTemporary.append(i, listOfItems.get(i));
 
-                if (listOfItems.get(i).getIsHomePage().equals(1)) {
-                    homeScreenId = menuId - 1;
-                }
-
-            } else if (menuType.equals("submenu")) {
-                type = listOfItems.get(i).getType();
-                title = listOfItems.get(i).getComponentTitle();
-
-                submenuArray.append(submenuId, new HelperComponent(type, title));
-                submenuId++;
-
-                if (listOfItems.get(i).getIsHomePage().equals(1)) {
-                    homeScreenId = menuId - 1;
-                }
+                    if (listOfItems.get(i).getIsHomePage().equals(1)) {
+                        homeScreenId = listOfItems.get(i).getId();
+                    }
+                    break;
             }
         }
+
+        int key;
+        int index = 0;
+        int position = 1;
+        do {
+            key = menuTemporary.keyAt(index);
+
+            if (menuTemporary.get(key).getPosition() == position) {
+                type = menuTemporary.get(key).getType();
+                title = menuTemporary.get(key).getComponentTitle();
+
+                menuArray.append(position - 1, new HelperComponent(type, title));
+
+                if (menuTemporary.get(key).getIsHomePage().equals(1)) {
+                    homeScreenId = menuTemporary.get(key).getPosition() - 1;
+                }
+
+                position++;
+                index = 0;
+            } else {
+                index++;
+            }
+        } while (menuArray.size() < menuTemporary.size());
+
+        index = 0;
+        position = 1;
+        do {
+            key = submenuTemporary.keyAt(index);
+
+            if (submenuTemporary.get(key).getPosition() == position) {
+                type = submenuTemporary.get(key).getType();
+                title = submenuTemporary.get(key).getComponentTitle();
+
+                submenuArray.append(position - 1, new HelperComponent(type, title));
+
+                if (submenuTemporary.get(key).getIsHomePage().equals(1)) {
+                    homeScreenId = submenuTemporary.get(key).getPosition() + menuArray.size() - 1;
+                }
+
+                position++;
+                index = 0;
+            } else {
+                index++;
+            }
+        } while (submenuArray.size() < submenuTemporary.size());
 
         if (view != null) {
             view.setDataToNavDrawer(menuArray, submenuArray, homeScreenId);
