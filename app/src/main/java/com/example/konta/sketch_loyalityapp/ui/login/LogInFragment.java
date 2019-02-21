fa1057063cc25e38860893e3e1fdaec5a361f3f8
SilentOne95 <silentone@onet.pl>
2019-02-21 11:50:22 +0100

    Creating new login layout

--- app/src/main/java/com/example/konta/sketch_loyalityapp/ui/login/LogInFragment.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/ui/login/LogInFragment.java
@@ -4,14 +4,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.example.konta.sketch_loyalityapp.R;
+import com.example.konta.sketch_loyalityapp.base.BaseActivity;
 import com.example.konta.sketch_loyalityapp.base.BaseFragment;
 
-public class LogInFragment extends BaseFragment implements View.OnClickListener {
-
-    private TextView mSignUpTextView;
+public class LogInFragment extends BaseFragment {
 
     @Override
     protected int getLayout() { return R.layout.fragment_log_in; }
@@ -20,12 +18,6 @@ public class LogInFragment extends BaseFragment implements View.OnClickListener 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mSignUpTextView = rootView.findViewById(R.id.sign_up_text_view);
-        mSignUpTextView.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-
+        ((BaseActivity) getActivity()).getSupportActionBar().hide();
     }
 }
