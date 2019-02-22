cc8b5e7ac2171df346426b17b25a1ac9a3c5174f
SilentOne95 <silentone@onet.pl>
2019-02-22 11:13:01 +0100

    Setting up logging in with Facebook custom button

--- app/src/main/java/com/example/konta/sketch_loyalityapp/ui/login/LogInFragment.java
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/ui/login/LogInFragment.java
@@ -11,6 +11,12 @@ import android.widget.Button;
 import com.example.konta.sketch_loyalityapp.R;
 import com.example.konta.sketch_loyalityapp.base.BaseActivity;
 import com.example.konta.sketch_loyalityapp.base.BaseFragment;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -18,9 +24,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
 
 import static com.example.konta.sketch_loyalityapp.Constants.RC_SIGN_IN;
 
@@ -28,10 +37,10 @@ public class LogInFragment extends BaseFragment implements View.OnClickListener 
 
     private static final String TAG = LogInFragment.class.getSimpleName();
 
-    private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private CallbackManager mCallbackManager;
     private Button mLogInWithGoogleButton, mLogInWithFacebookButton, mLogInWithPhoneButton;
-
 
     @Override
     protected int getLayout() { return R.layout.fragment_log_in; }
@@ -40,22 +49,45 @@ public class LogInFragment extends BaseFragment implements View.OnClickListener 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mLogInWithGoogleButton = rootView.findViewById(R.id.login_google_button);
-        mLogInWithGoogleButton.setOnClickListener(this);
-
-        mLogInWithFacebookButton = rootView.findViewById(R.id.login_facebook_button);
-        mLogInWithFacebookButton.setOnClickListener(this);
-
-        mLogInWithPhoneButton = rootView.findViewById(R.id.login_phone_button);
-        mLogInWithPhoneButton.setOnClickListener(this);
-
+        // Hide action bar
         ((BaseActivity) getActivity()).getSupportActionBar().hide();
 
+        // Views
+        mLogInWithGoogleButton = rootView.findViewById(R.id.login_google_button);
+        mLogInWithFacebookButton = rootView.findViewById(R.id.login_facebook_button);
+        mLogInWithPhoneButton = rootView.findViewById(R.id.login_phone_button);
+
+        // Button listeners
+        mLogInWithGoogleButton.setOnClickListener(this);
+        mLogInWithFacebookButton.setOnClickListener(this);
+        mLogInWithPhoneButton.setOnClickListener(this);
+
+        // Log In with Google
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.server_client_id))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(getContext(), googleSignInOptions);
+
+        // Log In with Facebook
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("test", "onSuccess");
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("test", "onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("test", "onError");
+            }
+        });
 
         // TODO: Create attach method
         // Temporary init FirebaseAuth in this fragment - testing purpose
@@ -70,6 +102,7 @@ public class LogInFragment extends BaseFragment implements View.OnClickListener 
                 googleSignIn();
                 break;
             case R.id.login_facebook_button:
+                facebookSignIn();
                 break;
             case R.id.login_phone_button:
                 break;
@@ -84,10 +117,15 @@ public class LogInFragment extends BaseFragment implements View.OnClickListener 
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    private void facebookSignIn() {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"));
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        // Google
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
@@ -99,12 +137,16 @@ public class LogInFragment extends BaseFragment implements View.OnClickListener 
                 Log.d(TAG, "Google sign in failed", e);
             }
         }
+
+        // Facebook
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
 
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(getActivity(), task -> {
                     if (task.isSuccessful()) {
@@ -114,6 +156,23 @@ public class LogInFragment extends BaseFragment implements View.OnClickListener 
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.d(TAG, "signInWithCredential:failure", task.getException());
+                    }
+                });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("test", "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.d("test", "signInWithCredential:failure", task.getException());
                     }
                 });
     }
