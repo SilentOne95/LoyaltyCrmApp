2fa176bbc5dd6b40f6b44b4ccbbe03e1384d9d72
SilentOne95 <silentone@onet.pl>
2019-03-20 11:05:38 +0100

    Creating RoomDatabase structure

---
+++ app/src/main/java/com/example/konta/sketch_loyalityapp/data/Word.java
@@ -0,0 +1,26 @@
+package com.example.konta.sketch_loyalityapp.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int mId;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(int id, @NonNull String word) {
+        this.mId = id;
+        this.mWord = word;
+    }
+
+    public int getId() { return this.mId; }
+
+    public String getWord() { return this.mWord; }
+}
