7a1d2a6bb575316f3e8046977ef4a6418650c833
SilentOne95 <silentone@onet.pl>
2019-07-08 13:58:16 +0200

    Created alert dialog in launch activity in case no network connection

--- app/src/main/java/com/sellger/konta/sketch_loyaltyapp/ui/launch/LaunchActivity.java
+++ app/src/main/java/com/sellger/konta/sketch_loyaltyapp/ui/launch/LaunchActivity.java
@@ -1,9 +1,16 @@
 package com.sellger.konta.sketch_loyaltyapp.ui.launch;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+
+import com.sellger.konta.sketch_loyaltyapp.R;
 import com.sellger.konta.sketch_loyaltyapp.ui.main.MainActivity;
 
 public class LaunchActivity extends AppCompatActivity {
@@ -11,7 +18,26 @@ public class LaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(LaunchActivity.this, MainActivity.class));
-        finish();
+        if (checkInternetConnection()) {
+            startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+            finish();
+        } else {
+            displayNoInternetAlertDialog();
+        }
+    }
+
+    private boolean checkInternetConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private void displayNoInternetAlertDialog() {
+        new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.settings_alert_dialog_log_out_style))
+                .setCancelable(true)
+                .setMessage(R.string.launcher_alert_dialog_message)
+                .setPositiveButton(R.string.launcher_alert_dialog_confirm, ((dialog, which) -> finish()))
+                .show();
     }
 }
